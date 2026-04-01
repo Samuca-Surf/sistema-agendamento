5f53eed42260f1bba12f098cf9b1632653e4e94d
samuelMesquita-sudo-su <samuel_mesquita@estudante.sesisenai.org.br>
2026-03-31 22:28:54 -0300

    asa

--- src/main/java/com/samuel/sistema_agendamento/agendamento/model/Usuario.java
+++ src/main/java/com/samuel/sistema_agendamento/agendamento/model/Usuario.java
@@ -1,8 +1,12 @@
 package com.samuel.sistema_agendamento.agendamento.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,41 +23,5 @@ public class Usuario {
 
 
     public Usuario() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(Integer telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 
 }
---
+++ src/main/java/com/samuel/sistema_agendamento/role/Rule.java
@@ -0,0 +1,5 @@
+package com.samuel.sistema_agendamento.role;
+
+public enum Rule {
+    ADMIN, USUARIO_COMUM, USUARIO_PREVILEGIADO, USUARIO_PAGANDO
+}
